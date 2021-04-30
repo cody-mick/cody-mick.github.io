ab8b6d3929577a1a862e866851aa8bc3150724fd
cody-mick <cmick@byui.edu>
2021-04-29 20:11:11 -0600

    messing with css

---
+++ lesson2/js/design-principles.js
@@ -0,0 +1,5 @@
+document.querySelector("#lastmodified").textContent =
+  document.lastModified; /* Used the code from the demonstration by Brother Blazzard */
+
+let year = new Date().getFullYear(); /* Snippet from W3 Schools */
+document.querySelector("#year").textContent = year;
