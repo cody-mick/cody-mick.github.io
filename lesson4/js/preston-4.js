a94541cf389a28ebe6a94c153be3ce35f0e0712c
cody-mick <cmick@byui.edu>
2021-05-13 15:09:41 -0600

    Added responsive menu

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -3,3 +3,15 @@ document.querySelector("#lastmodified").textContent =
 
 let year = new Date().getFullYear(); /* Snippet from W3 Schools */
 document.querySelector("#year").textContent = year;
+
+/* Code from the CodePen in the W04 Activities. Subject to change. */
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
