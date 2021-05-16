a67a68275b281a2edf1ea8a256aacba70f97e7a4
cody-mick <cmick@byui.edu>
2021-05-15 22:44:53 -0600

    Added todaysdate js

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,8 +1,22 @@
 document.querySelector("#lastmodified").textContent =
   document.lastModified; /* Used the code from the demonstration by Brother Blazzard */
 
-let year = new Date().getFullYear(); /* Snippet from W3 Schools */
+let date = new Date();
+
+let year = date.getFullYear(); /* Snippet from W3 Schools */
 document.querySelector("#year").textContent = year;
+
+let day = date.getDay();
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let month = date.getMonth();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let number = date.getDate();
+
+let todaysDate = weekdays[day] + ", " + number + " " + months[month] + " " + year;
+
+document.querySelector("#currentdate").textContent = todaysDate;
 
 /* Code from the CodePen in the W04 Activities. Subject to change. */
 const hambutton = document.querySelector(".ham");
