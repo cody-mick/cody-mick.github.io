a2bf7909b294e35096ffde63b08ec9d504512639
cody-mick <cmick@byui.edu>
2021-05-20 14:20:17 -0600

    finished DOM manipulation assignment

---
+++ lesson5/js/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ul.list");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let item = input.value;
+    input.value = "";
+  
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const deletebutton = document.createElement("button");
+  
+    listItem.appendChild(listText);
+    listText.textContent = item;
+    listItem.appendChild(deletebutton);
+    deletebutton.textContent = "❌";
+    list.appendChild(listItem);
+  
+    deletebutton.onclick = function (e) {
+        list.removeChild(listItem);
+    }
+  
+    input.focus();
+})
