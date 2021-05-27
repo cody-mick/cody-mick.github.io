8a757eda197b90141f16e3a48d511dd8a157f07a
cody-mick <cmick@byui.edu>
2021-05-27 14:10:30 -0600

    created lesson6 folder to begin assignment

---
+++ lesson6/js/preston-6.js
@@ -0,0 +1,63 @@
+let date = new Date();
+
+let year = date.getFullYear(); /* Snippet from W3 Schools */
+document.querySelector("#year").textContent = year;
+
+let day = date.getDay();
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let month = date.getMonth();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let number = date.getDate();
+
+let todaysDate =
+  weekdays[day] + ", " + number + " " + months[month] + " " + year;
+
+document.querySelector("#currentdate").textContent = todaysDate;
+
+/* Code from the CodePen in the W04 Activities. Subject to change. */
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+let banner = document.querySelector("#banner");
+document.addEventListener("DOMContentLoaded", () => {
+  if (date.getDay() == 6) banner.style.display = "block"
+});
+
+// let bannerDate = new Date();
+// if (bannerDate.getDay == 6) {
+//   document.querySelector("#banner").style.display = "block";
+// }
+// else {
+//   document.querySelector("#banner").style.display = "none";
+// }
