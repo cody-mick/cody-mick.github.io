33992bedf6c79d1af37ba9ab553ccce2ae22c9cf
cody-mick <cmick@byui.edu>
2021-06-23 10:58:49 -0600

    used fahrenheit units

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,5 +1,5 @@
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=7a8d08997888f7d4fa1ce88d0e56a068";
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=7a8d08997888f7d4fa1ce88d0e56a068";
 
 fetch(apiURL)
   .then((response) => response.json())
