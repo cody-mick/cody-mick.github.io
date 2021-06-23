4eba35f6545acacc8e837fbeab07ebf29ae64467
cody-mick <cmick@byui.edu>
2021-06-23 10:44:24 -0600

    added api test to assignment portal

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,8 +1,14 @@
 const apiURL =
-  "api.openweathermap.org/data/2.5/weather?id=5604473&appid=7a8d08997888f7d4fa1ce88d0e56a068";
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=7a8d08997888f7d4fa1ce88d0e56a068";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
   });
