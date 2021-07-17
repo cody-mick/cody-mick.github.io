ea513d38e78dc48de2b2f7c4b61b30500fa91f12
cody-mick <cmick@byui.edu>
2021-07-17 13:42:54 -0600

    Looking alright

--- chamber-of-commerce/js/home-page.js
+++ chamber-of-commerce/js/home-page.js
@@ -1,3 +1,4 @@
+// Tutorial from Dev Ed on YouTube
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
@@ -12,12 +13,49 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
       }
     });
     // Burger Animation
-    burger.classList.toggle('toggle');
+    burger.classList.toggle("toggle");
   });
 };
 
 navSlide();
+
+// Last modified in the footer
+document.querySelector(".last-modified").textContent =
+  document.lastModified; /* Used the code from the demonstration by Brother Blazzard */
+
+let year = new Date().getFullYear(); /* Snippet from W3 Schools */
+document.querySelector(".year").textContent = year;
+
+// Weather Information
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=43.67227300668368&lon=-111.91471974451605&units=imperial&exclude=minutely,hourly&appid=7a8d08997888f7d4fa1ce88d0e56a068"
+
+fetch(url)
+  .then((response) => response.json())
+  .then((forecast) => {
+    // Verify the data was successfully retrieved.
+    console.log(forecast);
+    
+    console.log(forecast.alerts[0].event);
+    console.log(forecast.alerts[0].start);
+    console.log(forecast.alerts[0].end);
+
+    let warningType = document.querySelector(".warning-type");
+    warningType.textContent = forecast.alerts[0].event;
+    let start = document.querySelector('.alert-start');
+    let end = document.querySelector('.alert-end');
+    start.textContent = new Date(forecast.alerts[0].start).getFullYear();
+    end.textContent = new Date(forecast.alerts[0].end).getFullYear();
+  });
+
+  let closeAlert = document.querySelector('.close-alert');
+  let weatherAlert = document.querySelector('.alert-banner');
+  closeAlert.addEventListener('click', () => {
+    closeAlert.style.display = "none";
+    weatherAlert.style.display = "none";
+  })
