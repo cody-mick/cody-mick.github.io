3b5967e5fad6f5916867e9aeccbf9093be18b864
cody-mick <cmick@byui.edu>
2021-07-20 22:20:49 -0600

    the beginnings of the directory page. Git her done son

--- chamber-of-commerce/js/directory.js
+++ chamber-of-commerce/js/directory.js
@@ -1,5 +1,17 @@
-let companyData = "";
+let companyData = "js/local-business.json";
+fetch(companyData)
+    .then((response) => response.json())
+    .then((businesses) => {
+        // Verify the retrieval of the data
+        console.log(businesses);
 
-const obj = JSON.parse(companyData);
-console.log(obj);
-
+        // Loop through the data and create the business cards
+        for (let i = 0; i < 9; i++) {
+            let card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            card.textContent = businesses.members[i].business;
+            
+            let directory = document.querySelector('.directory');
+            directory.appendChild(card);
+        }
+    })
