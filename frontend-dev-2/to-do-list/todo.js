3fe4dfab2ae27f98b87dbb9e22cdc1b32d97c983
cody-mick <cmick@byui.edu>
2021-10-25 18:03:00 -0600

    what the heck man
    Co-authored-by: Reece Poulsen <Reecepoulsen@users.noreply.github.com>

--- frontend-dev-2/to-do-list/todo.js
+++ frontend-dev-2/to-do-list/todo.js
@@ -1,27 +1,47 @@
-function addTask() {
-  let todoList = document.querySelector(".to-do-list");
+const todoList = document.querySelector(".to-do-list");
+let toDos = Array.from(todoList.children);
+console.log(toDos);
+
+function addTask(todoList) {
   let newTaskValue = document.getElementById("task");
 
   if (newTaskValue.value != "") {
     let newTask = document.createElement("li");
+
     let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "delete");
+    deleteBtn.setAttribute("onclick", "removeTask(todoList)");
     deleteBtn.textContent = "X";
+
+    let checkBtn = document.createElement("button");
+    checkBtn.setAttribute("class", "complete");
+    checkBtn.setAttribute("onclick", "completeTask(todoList)");
+    checkBtn.textContent = "\u2713";
+
     newTask.textContent = newTaskValue.value;
     newTask.appendChild(deleteBtn);
+    newTask.appendChild(checkBtn);
     todoList.appendChild(newTask);
     newTaskValue.value = "";
   } else {
-    alert('"If you plan to do nothing, you will get nothing done." -Confucious...probably');
+    alert(
+      '"If you plan to do nothing, you will get nothing done." -Confucious...probably'
+    );
   }
 }
 
-function removeTask() {
+function removeTask(todoList) {
 
 }
 
-
 function filter(type) {
-    if (type == "completed") {
-      let completedTasks = document.querySelectorAll(".checked");
-    }
+  if (type == "completed") {
+    let completedTasks = document.querySelectorAll(".checked");
+  }
+}
+
+export default class Tasks {
+  constructor(elementId) {
+    
+  }
 }
