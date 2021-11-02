e1b6ed731f1c2a3aa2ae5211ad6f75fe3d773730
cody-mick <cmick@byui.edu>
2021-11-02 11:45:07 -0600

    finished team of activity

--- frontend-dev-2/js/main.js
+++ frontend-dev-2/js/main.js
@@ -1,41 +1,45 @@
 const links = [
-    {
-        label: "Week 1",
-        url: "week1/index.html"
-    },
-    {
-        label: "Week 2",
-        url: "week2/index.html"
-    },
-    {
-        label: "Week 3",
-        url: "week3/index.html"
-    },
-    {
-        label: "Week 4",
-        url: "week4/index.html"
-    },
-    {
-        label: "Week 5",
-        url: "week5/index.html"
-    },
-    {
-        label: "To Do List",
-        url: "to-do-list/index.html"
-    },
-    {
-        label: "Week 7",
-        url: "week7/index.html"
-    }
+  {
+      label: "Week 1",
+      url: "week1/index.html"
+  },
+  {
+      label: "Week 2",
+      url: "week2/index.html"
+  },
+  {
+      label: "Week 3",
+      url: "week3/index.html"
+  },
+  {
+      label: "Week 4",
+      url: "week4/index.html"
+  },
+  {
+      label: "Week 5",
+      url: "week5/index.html"
+  },
+  {
+      label: "To Do List",
+      url: "to-do-list/index.html"
+  },
+  {
+      label: "Week 7",
+      url: "week7/index.html"
+  },
+  {
+    label: "Week 8",
+    url: "week8/index.html"
+  }
 ]
 
 let contents = document.querySelector('.contents');
 
 for (let i = 0; i < links.length; i++) {
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    a.setAttribute('href', links[i].url);
-    a.innerHTML = links[i].label;
-    li.appendChild(a);
-    contents.appendChild(li);
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', links[i].url);
+  a.innerHTML = links[i].label;
+  li.appendChild(a);
+  contents.appendChild(li);
 }
